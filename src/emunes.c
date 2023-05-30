7257338492a58b9c3392afaa6a210be1619c299e
Keith Leonardo <kleonardo52@gmail.com>
2023-05-30 23:29:09 +1000

    Implement ROM loader for init

--- src/emunes.c
+++ src/emunes.c
@@ -1,4 +1,15 @@
+#include "./init.h"
+
 int main(int argc, char **argv) {
-    // let's git it
+    // Load a ROM into memory
+    rom_t rom = parse_args(argc, argv);
+    if (rom.buffer == NULL) {
+        return 1;
+    }
+
+    // TODO: Execute instructions in main loop
+
+    // Cleanup
+    free_rom(&rom);
     return 0;
 }
