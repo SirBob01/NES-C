2f9e9068e91b69ae2509d4c5588b1e23e1d5e6b0
Keith Leonardo <kleonardo52@gmail.com>
2023-05-31 22:03:17 +1000

    Fix cpu init

--- src/cpu.c
+++ src/cpu.c
@@ -2,11 +2,12 @@
 
 cpu_t create_cpu() {
     cpu_t cpu;
-    cpu.registers.p = 0x34;
     cpu.registers.a = 0;
     cpu.registers.x = 0;
     cpu.registers.y = 0;
-    cpu.registers.s = 0xfd;
+    cpu.registers.pc = 0;
+    cpu.registers.s = 0;
+    cpu.registers.p = 0;
     cpu.cycles = 0;
     cpu.memory = allocate_memory(CPU_RAM_SIZE);
     return cpu;
