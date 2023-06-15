81c1a0422fbc478119b0885ba00a58ec139453c6
Keith Leonardo <kleonardo52@gmail.com>
2023-06-15 10:21:18 +1000

    Remove CPU_MEMORY_MAP array, use values directly

--- src/cpu.h
+++ src/cpu.h
@@ -7,37 +7,21 @@
 // 6502 has a 16-bit address bus (64k)
 #define CPU_RAM_SIZE 1 << 16
 
-// Indices to the CPU_MEMORY_MAP array
-#define CPU_MAP_RAM          0
-#define CPU_MAP_MIRROR_0     1
-#define CPU_MAP_MIRROR_1     2
-#define CPU_MAP_MIRROR_2     3
-#define CPU_MAP_PPU_REG      4
-#define CPU_MAP_PPU_MIRROR   5
-#define CPU_MAP_APU_IO       6
-#define CPU_MAP_APU_IO_DEBUG 7
-#define CPU_MAP_CARTRIDGE    8
+// CPU memory map address offsets
+#define CPU_MAP_RAM          0x0000
+#define CPU_MAP_MIRROR_0     0x0800
+#define CPU_MAP_MIRROR_1     0x1000
+#define CPU_MAP_MIRROR_2     0x1800
+#define CPU_MAP_PPU_REG      0x2000
+#define CPU_MAP_PPU_MIRROR   0x2008
+#define CPU_MAP_APU_IO       0x4000
+#define CPU_MAP_APU_IO_DEBUG 0x4018
+#define CPU_MAP_CARTRIDGE    0x4020
 
 // Interrupt vector positions
 #define CPU_VEC_NMI     0xfffa
 #define CPU_VEC_RESET   0xfffc
 #define CPU_VEC_IRQ_BRK 0xfffe
-
-/**
- * @brief CPU memory map offsets.
- *
- */
-static const address_t CPU_MEMORY_MAP[] = {
-    0x0000, // 2k RAM
-    0x0800, // Mirror 0
-    0x1000, // Mirror 1
-    0x1800, // Mirror 2
-    0x2000, // PPU registers
-    0x2008, // Mirror of 0x2000-0x2007 (every 8 bytes)
-    0x4000, // APU I/O
-    0x4018, // APU I/O (debug)
-    0x4020, // Cartridge
-};
 
 /**
  * @brief CPU status flags.
