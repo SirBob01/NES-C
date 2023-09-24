23155370205da7e340a2147cee61150966189886
Keith Leonardo <kleonardo52@gmail.com>
2023-09-25 00:53:57 +1000

    Fix NMI signal timing

--- tests/src/ppu-test.c
+++ tests/src/ppu-test.c
@@ -16,9 +16,8 @@ static char *test_blargg_ppu_vbl_nmi() {
         "../roms/ppu_vbl_nmi/02-vbl_set_time.nes",
         "../roms/ppu_vbl_nmi/03-vbl_clear_time.nes",
         "../roms/ppu_vbl_nmi/04-nmi_control.nes",
-
-        // TODO: Fix timing
         "../roms/ppu_vbl_nmi/05-nmi_timing.nes",
+        // TODO: Handle suppression
         "../roms/ppu_vbl_nmi/06-suppression.nes",
         "../roms/ppu_vbl_nmi/07-nmi_on_timing.nes",
         "../roms/ppu_vbl_nmi/08-nmi_off_timing.nes",
@@ -33,7 +32,7 @@ static char *test_blargg_ppu_vbl_nmi() {
     unsigned char status = 0;
     char result[2048] = {0};
 
-    for (unsigned i = 0; i < 7; i++) {
+    for (unsigned i = 0; i < 8; i++) {
         emulator_t emu;
         create_emulator(&emu, test_roms[i]);
 
